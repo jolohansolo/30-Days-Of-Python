704786a86cdbeaffc1b3c938cedfcf3d6d0bf9ec
jolohansolo <koziarjulian@gmail.com>
2026-07-10 10:14:14 +0200

    Add files via upload

---
+++ 03_Day_Operators/day3.py
@@ -0,0 +1,34 @@
+
+age = 19
+height = 1.83
+cmplx=1+2j
+#base = input("podaj podstawe")
+#height = input('podaj wysokosc')
+#area = int(base) * int(height) / 2
+#print (area)
+#a=input('a')
+#b=input('b')
+#c=input('c')
+#perimeter=int(a)+int(b)+int(c)
+#print("the perimeter of triangle is", perimeter)
+#print( len("dragon") <= len("python"))
+#print(('on' in 'python') and ('on' in 'dragon') )
+#print( 'jargon' in "i hope this course is not full of jargon")
+#print(('on' in 'python') or ('on' in 'dragon') )
+#txt=len('python')
+#float(txt)
+#str(txt)
+#print (txt)
+#x=input('dej liczbe')
+#print(float(x)%2==0)
+#zrób 21 22 23
+#rate_per_hour=input('ile za godzinę')
+#hours=input('ile godzin')
+#print('dostaniesz w tygodniu ',int(hours)*float(rate_per_hour))
+#lata=input('ile żyjesz lat?')
+#print('zyjesz: ',int(lata)*365*24*60*60,'sekund')
+for i in range(1,6):
+    print(i)
+    for j in range(4):
+        print(i**j)
+
